4772ae2a827f3c8f574f412c4b46b19a3005248a
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-05-30 17:05:19 -0500

    store implements ok

---
+++ server/src/application/store/store.usecases.ts
@@ -0,0 +1,10 @@
+import { StoreRepository } from '@domain/repositories/store.repository';
+import { StoreEntity } from '@domain/entities/store.entity';
+
+export class StoreUseCases {
+    constructor(private readonly storeRepo: StoreRepository) {}
+
+    async findAll(): Promise<StoreEntity[] | null> {
+        return this.storeRepo.findAll();
+    }
+}
